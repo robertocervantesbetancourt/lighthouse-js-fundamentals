cec934c3f23bd4b9e7b1efc0e2d4c9a487e2c007
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-02-15 19:17:40 +0000

    old exercise

---
+++ tinkering.js
@@ -0,0 +1,11 @@
+/*function multiply(a,b){
+  return a*b;
+};
+
+const result = multiply(2,4);
+
+console.log(result);*/
+
+const my_string = 'b';
+const ASCII_value = my_string.charCodeAt(0);
+console.log(ASCII_value);
